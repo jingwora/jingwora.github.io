032d12d0ad486374a08ed1d06275cbbc57a9f6bf
jingwora <jingwora@gmail.com>
2023-03-12 23:37:04 +0900

    update

--- contents/projects/predict-demo/predict.js
+++ contents/projects/predict-demo/predict.js
@@ -3,17 +3,18 @@
 // const tf = require("@tensorflow/tfjs-node");
 
 const tf = require("@tensorflow/tfjs");
+// const fs = require("fs");
 
-model1 = tf.loadLayersModel(
+const model = tf.loadLayersModel(
   "https://raw.githubusercontent.com/jingwora/jingwora.github.io/master/contents/projects/predict-demo/odd-engine-v1.00.6/model.json"
 );
 
 // vig  prob1  prob2  prob3
 // 0.12    0.7   0.31   0.82
 
-input_xs = tf.tensor2d([[0.12, 0.7, 0.31, 0.82]]);
+const input_xs = tf.tensor2d([[0.12, 0.7, 0.31, 0.82]]);
 
-output = model1.predict(input_xs);
+const output = model1.predict(input_xs);
 
 const outputData = output.dataSync();
 
